5e9ebe54619a74c18729367a72aad30abcc782aa
antoniojunior1890 <antonio.junior1890@hotmail.com>
2019-10-08 23:01:08 -0300

    logo tela List

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import{ View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import{ SafeAreaView, Platform, Image, StyleSheet, AsyncStorage } from 'react-native';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
-    return <View>
-        <Text>Ola</Text>
-    </View>
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, []);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? 25 : 0,
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 10
+    },
+});
